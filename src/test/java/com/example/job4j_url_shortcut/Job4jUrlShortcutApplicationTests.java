e3b073e220d4ba0864f9c640105ea7460bf5daf1
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-09-21 10:24:18 +0300

    Initial commit

---
+++ src/test/java/com/example/job4j_url_shortcut/Job4jUrlShortcutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.job4j_url_shortcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jUrlShortcutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
